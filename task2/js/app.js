047aad1edde35d44ef746fd011692cf5fcdce9e9
bohdanbirdie <voja4ok@yahoo.com>
2017-01-05 05:33:37 +0200

    task 2 update

--- task2/js/app.js
+++ task2/js/app.js
@@ -1,17 +1,18 @@
 // setInterval(function(){
 // debugger; 
 var numbers = 0;
-var counter = 0;
+var counter = [0, 0, 0, 0, 0, 0];
+
 setInterval(function count(grade) {
   // debugger; 
   var grade = grade || 0;
   // var step = grade == 0 ? 2 : 1;
   var step = 2;
   var highestNumber = grade % 2 == 0 ? 9 : 6;
-  var flag = false;
+  var flag = [false, false, false, false, false, false];
 
   $('.digit-' + grade + '[data-position]').each(function() { //get in eacth digit-N
-    if ($(this).attr("data-position") == 'top') {
+    if ($(this).attr("data-position") == 'top') { //if this element if top - set it to boottom and do changes
 
 
 
@@ -22,16 +23,16 @@ setInterval(function count(grade) {
       $(this).children().each(function() { //loop thru all digit-X div's childrens
         // debugger;
         // $(this).each(function() { //loop thru
-          if ($(this).children().text() <= (highestNumber-1)) {
-            $(this).children().text(+$(this).children().text() + step);
-          } else {
-            // debugger
-            // console.log($(this).children().text());
-            // flag = true;
-            // console.log(numbers);
-            $(this).children().text(+$(this).children().text() - (highestNumber - 1));
+        if ($(this).children().text() <= (highestNumber - 1)) { //if number is lower then highest number for current digit
+          $(this).children().text(+$(this).children().text() + step); // do one more step
+        } else { // else set the overflow flag and reset number 
+          // debugger
+          // console.log($(this).children().text());
+          flag[grade] = true;
+          // console.log(numbers);
+          $(this).children().text(+$(this).children().text() - (highestNumber - 1));
 
-          }
+        }
         // });
       });
 
@@ -39,21 +40,21 @@ setInterval(function count(grade) {
       $(this).children().eq(2).removeClass('before-half');
       $(this).children().eq(3).removeClass('after-half');
 
-    } else {
+    } else {//current element is bottom - set it to top
 
       $(this).attr("data-position", 'top');
 
       $(this).children().each(function() { //get in each
         // debugger;
         // $(this).each(function() {
-          if ($(this).children().text() <= highestNumber) {
-            // $(this).children().text(+$(this).children().text());//dont need this rigth now
-          } else {
-            // console.log($(this).children().text());
-            // flag = true;
-            $(this).children().text(+$(this).children().text() - (highestNumber + 1));
+        if ($(this).children().text() <= highestNumber) {
+          // $(this).children().text(+$(this).children().text());//dont need this rigth now
+        } else {
+          // console.log($(this).children().text());
+          // flag[grade] = true;
+          $(this).children().text(+$(this).children().text() - (highestNumber + 1));
 
-          }
+        }
         // });
       });
 
@@ -62,88 +63,22 @@ setInterval(function count(grade) {
       $(this).css("z-index", "1");
       $(this).children().eq(2).addClass('before-half');
       $(this).children().eq(3).addClass('after-half');
-    }//else end
+    } //else end
 
 
 
 
   });
 
+  console.log(flag[grade]);
+
+  if (flag[grade]) {
+    if ((counter[grade] % 2) == 0) {
+      count(grade + 1);
+    }
+    counter[grade] += 1;
+
+  }
 
 
-      if (flag) {
-        if ((counter % 2) == 0) {
-          count(grade + 1);
-        }
-        counter++;
-      }
-
-
-}, 500);
-
-
-
-// setInterval(function count2(grade){
-//   numbers +=1;
-//   console.log(numbers);
-//   // debugger; 
-//   var grade = grade || 0;
-//   var step = grade == 0 ? 2 : 1;
-//   var highestNumber = grade % 2 == 0 ? 2 : 9;
-//   console.log('inside');
-//   // debugger;
-
-//    //get in eacth digit-N
-
-    
-
-//     // if ($(this).attr("data-position") == 'top') {
-
-//     //   console.log('inside first if');
-
-//     //   $(this).attr("data-position", 'bottom');
-
-//     //   console.log('position changed');
-
-//       $('.digit-1[data-position]').first().toggleClass('top-index');
-//       $('.digit-1[data-position]').first().toggleClass('bottom-index');
-
-//       $('.digit-1[data-position]').first().children().each(function() { //get in each
-//         // debugger;
-//         $(this).each(function() {
-//           if ($(this).children().text() < highestNumber) {
-//             $(this).children().text(+$(this).children().text() + step+1);
-//           } else {
-//             $(this).children().text(+$(this).children().text() - (highestNumber - 1));
-//           }
-//         });
-//       });
-
-//       $('.digit-1[data-position]').first().children().eq(2).toggleClass('before-half');
-//       $('.digit-1[data-position]').first().children().eq(3).toggleClass('after-half');
-
-//     // } else {
-
-//       // $(this).attr("data-position", 'top');
-
-//       $('.digit-1[data-position]').next().children().each(function() { //get in each
-//         // debugger;
-//         $(this).each(function() {
-//           if ($(this).children().text() <= highestNumber) {
-//             $(this).children().text(+$(this).children().text() + step);
-//             // $(this).children().text(+$(this).children().text());//dont need this rigth now
-//           } else {
-
-//             $(this).children().text(+$(this).children().text() - (highestNumber + 1));
-
-//           }
-//         });
-//       });
-
-//       $('.digit-1[data-position]').next().toggleClass('top-index');
-//       $('.digit-1[data-position]').next().toggleClass('bottom-index');
-//       $('.digit-1[data-position]').next().children().eq(2).toggleClass('before-half');
-//       $('.digit-1[data-position]').next().children().eq(3).toggleClass('after-half');
-
-//     //else end
-// }, 1000);
+}, 400);
