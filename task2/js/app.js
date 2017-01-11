fb5decedb86b2ad97d768e821199719361590f0f
bohdanbirdie <voja4ok@yahoo.com>
2017-01-12 01:08:54 +0200

    indentation fixes in task 2

--- task2/js/app.js
+++ task2/js/app.js
@@ -1,4 +1,3 @@
-// setInterval(function(){
 // debugger;
 var numbers = 0;
 var counter = [0, 0, 0, 0, 0, 0];
@@ -7,16 +6,14 @@ var counter = [0, 0, 0, 0, 0, 0];
 
 setBaseTime();
 
-function setBaseTime() {
+function setBaseTime() { //Set the base time and date from data-countdown-start
   var maxGrade = 7;
   var highestNumber;
   var baseTime = $('.wrapper').attr('data-countdown-start');
-  baseTime = baseTime.replace(/:/g, '').split('').reverse().join('');
+  baseTime = baseTime.replace(/:/g, '').split('').reverse().join(''); //covert input time string to number and use it as array
 
-  for (var i = 0; i <= maxGrade - 2; i++) {
+  for (var i = 0; i <= maxGrade - 2; i++) {//set the time without date, cause date has higher highestNumber
     highestNumber = i % 2 == 0 ? 9 : 5;
-    // console.log(baseTime[i]);
-    // console.info(i);
     $('.real-' + i).each(function() {
       $(this).text(+baseTime[i] + 1);
     });
@@ -33,10 +30,8 @@ function setBaseTime() {
   }
 
   // debugger;
-  for (var i = maxGrade - 1; i <= maxGrade; i++) {
+  for (var i = maxGrade - 1; i <= maxGrade; i++) {//set only date with correct highestNumber
     highestNumber = 9;
-    // console.log(baseTime[i]);
-    // console.info(i);
     $('.real-' + i).each(function() {
       $(this).text(+baseTime[i] + 1);
     });
@@ -44,8 +39,6 @@ function setBaseTime() {
       $(this).text(+baseTime[i]);
     });
     $('.double-next-' + i).each(function() {
-      console.log(baseTime[i]);
-
       if (baseTime[i] == 0) {
         $(this).text(highestNumber - 1);
       } else {
@@ -53,16 +46,13 @@ function setBaseTime() {
       }
     });
   }
-
-
 }
 
 
 
 setInterval(function count(grade) {
   // debugger; 
-  var grade = grade || 0;
-  // var step = grade == 0 ? 2 : 1;
+  var grade = grade || 0;//will be 0 for lowest grade digit
   var step = 2;
   var highestNumber;
   if (grade === 6 || grade === 7) {
@@ -70,47 +60,35 @@ setInterval(function count(grade) {
   } else {
     highestNumber = grade % 2 == 0 ? 9 : 5;
   }
-  // console.log(highestNumber);
-  // var lowestNumber = grade % 2 == 0 ? 9 : 6;
-  var flag = [false, false, false, false, false, false];
+
+  var flag = [false, false, false, false, false, false];//array for overflow flags
 
   $('.digit-' + grade + '[data-position]').each(function() { //get in eacth digit-N;
     if ($(this).attr("data-position") == 'top') { //if this element if top - set it to boottom and do changes
-
       $(this).attr("data-position", 'bottom');
-
-      // $(this).css("z-index", "0");
       $(this).removeClass('top-index');
       $(this).addClass('bottom-index');
+      $(this).children().each(function() { //loop thru all current digit div's childrens
 
-
-      $(this).children().each(function() { //loop thru all digit-X div's childrens
-
-
+        // IMPORTANT
         // if ($(this).children().text() <= (highestNumber - 1)) { //use this for not reversed timer
+
         if ($(this).children().text() > (0)) { //if number is lower then highest number for current digit
           $(this).children().text(+$(this).children().text() - step); // do one more step (change to '+' for not reversed timer)
         } else { // else set the overflow flag and reset number 
-
-
           flag[grade] = true;
 
+          // IMPORTANT
           // $(this).children().text(+$(this).children().text() - (highestNumber - 1));//use this for not reversed timer
           $(this).children().text(highestNumber - 1);
-
         }
       });
-
-
       $(this).children().eq(2).removeClass('before-half');
       $(this).children().eq(3).removeClass('after-half');
-
     } else { //current element is bottom - set it to top
-
       $(this).attr("data-position", 'top');
-
       $(this).children().each(function() { //loop thru all childrens of current element
-
+        // IMPORTANT
         // if ($(this).children().text() <= highestNumber) {//use this for not reversed timer
         if ($(this).children().text() > -1) {
           // $(this).children().text(+$(this).children().text());//dont need this rigth now
@@ -118,22 +96,17 @@ setInterval(function count(grade) {
           $(this).children().text(+$(this).children().text() + (highestNumber + 1));
         }
       });
-      // $(this).css("z-index", "1");
       $(this).removeClass('bottom-index');
       $(this).addClass('top-index');
       $(this).children().eq(2).addClass('before-half');
       $(this).children().eq(3).addClass('after-half');
     } //else end
   });
-  // console.log(flag[grade]);
 
-  if (flag[grade]) {
-    if ((counter[grade] % 2) == 0) {
+  if (flag[grade]) {//call recursion for higher grade digit
+    if ((counter[grade] % 2) == 0) {//this block prevent double function call when flag is true (not a bug, feature)
       count(grade + 1);
     }
     counter[grade] += 1;
-
   }
-
-
 }, 1000);
